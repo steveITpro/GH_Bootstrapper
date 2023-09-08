a6d1d043d74f907de4253269a8b8aef0dead75ba
steveITpro <43075589+steveITpro@users.noreply.github.com>
2023-09-08 21:52:20 +0100

    Create create_repo.sh

---
+++ GitHub/create_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO_NAME=$1
+
+# Create a new repository
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
+     -d "{\"name\":\"$REPO_NAME\", \"private\":false}" \
+     https://api.github.com/user/repos
+
+# If you want to initialize the repository, you can clone it, add a README or any other files, commit, and push.
